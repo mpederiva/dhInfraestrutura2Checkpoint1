772dc9ca75e36853e8209e01f55eb9a3915ae42e
Marta Pederiva <80361071+mpederiva@users.noreply.github.com>
2022-12-05 14:40:31 -0300

    Upload checkpoint 1

---
+++ output.tf
@@ -0,0 +1,10 @@
+output "instancias_ec2_docker" {
+  value = aws_instance.marta_ec2.*.public_ip
+}
+output "instancias_ec2_private" {
+  value = aws_instance.marta_ec2.*.private_ip
+}
+
+output "instancias_ec2_ansible" {
+  value = aws_instance.marta_ec2_ansible.*.public_ip
+}
